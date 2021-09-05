f620d89c0a6d127e3e2445856fcafdbe30c198c3
utifmd <utifmd@gmail.com>
2021-09-05 12:34:27 +0700

    solved major bugs

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
 <?php
     session_start();
 
-    $mysqli = new Mysqli("mysql", "root", "9809poiiop", "sik", 3306);
-    // $mysqli = new Mysqli("localhost", "root", "", "fadhila", 3306); 
+    $mysqli = new Mysqli("mysql", "root", "9809poiiop", "sik", 3306); // $mysqli = new Mysqli("localhost", "root", "", "fadhila", 3306); 
+    $list_month = array("Pilih Bulan", "Januari", "Februari", "Maret", "April", "Mai", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"); // date('F', mktime(0, 0 ,0 , $i, 1, date('Y')));
     $app_name = "RSIA Fadhila Batusangkar";
     $var_date_length = 31;
-
+    $total_summary = 0;
+    
     $query = $mysqli->query("SELECT * FROM poliklinik WHERE status='1'");
 
     if(mysqli_connect_errno()) exit();
-    else if(!$query) throw new Exception("Error Querying Request", 1);
-    
-    $list_month = []; for($i = 1; $i <= 12; $i++)
-    $list_month[] = date('F', mktime(0, 0 ,0 , $i, 1, date('Y')));
+    else if(!$query) throw new Exception("Error Querying Request", 1);    
     
     if(isset($_GET['year']))
         $current_year = $_GET['year'];
     else 
-        $current_year =date('Y');
+        $current_year = date('Y');
 
     if(isset($_GET['month'])){
         $current_month = $list_month[intval($_GET['month'])];
         $current_month_pos = $_GET['month'] ?: date('m'); 
     }else{
-        $current_month = date('F');
-        $current_month_pos = date('m'); 
+        $current_month = $list_month[0];// date('F');
+        $current_month_pos = 0; // date('m'); 
     }
     
     /*$current_year = $_GET['year'] ?: date('Y');
@@ -57,12 +55,12 @@
         <div class="collapse navbar-collapse" id="navbarRightAlignExample">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item"> 
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Home</a>
+                <a class="nav-link" href="./" tabindex="-1">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" data-mdb-toggle="modal"
+                <a class="nav-link" aria-current="page" data-mdb-toggle="modal"
                     data-mdb-target="#staticBackdrop" style="cursor:pointer">
-                <?php if($_SESSION['isSignedIn']) echo "Sign Out";
+                <?php if(isset($_SESSION['isSignedIn'])) echo "Sign Out";
                     else echo "Sign In"; ?>
                 </a>
             </li>
@@ -71,62 +69,76 @@
     </div>
     </nav>
     
-    <div class="p-5 text-center bg-light">
+    <div class="p-5 text-center">
         <h1 class="mb-3"><?php echo $app_name; ?></h1>
         <h4 class="mb-3">Aplikasi Laporan Registrasi Poliklinik</h4>
     </div>
-<?php if(!$_SESSION['isSignedIn']) { ?>
-    <div class="container p-3">
-        <div class="alert alert-warning" role="alert"> <?php switch ($_GET['invalid_code']) {
+<?php if(!isset($_SESSION['isSignedIn'])) { ?>
+    <div class="container">
+        <div class="alert alert-warning mb-5" role="alert"> <?php switch ($_GET['invalid_code']) {
             case 101: echo 'Kombinasi password dan username anda salah, pastikan anda menggunakan akun yang benar /valid.'; break;
             case 102: echo 'Panjang karakter username atau password harus 8-20'; break;
             default: echo 'Silahkan lakukan login dengan akun yang valid pada menu "Sign In" yang terdapat pada menu bar diatas.'; break; } ?>
         </div>
     </div>
 <?php } ?>
-    <div class="container-fluid p-3">
-        <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-        <div class="form-outline">
-        <input type="number" id="yearNumber" class="form-control" min="1800"
-            max="<?php echo date('Y') ?>" value="<?php echo $current_year ?>"/>
-        <label class="form-label" for="yearNumber">Year</label>
-        </div>
-        <div class="btn-group" role="group">
-            <button id="monthNumber" type="button" class="btn btn-primary dropdown-toggle"
-                data-mdb-toggle="dropdown" aria-expanded="false">
-                <?php echo $current_month ?>
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-            <?php for($i = 0; $i < count($list_month); $i++){
-                $selected_month = $list_month[$i]; echo 
-                "<li onclick=\"funApply('".$selected_month."', '".$i."')\"><a class=\"dropdown-item\">".$selected_month."</a></li>"; } ?>
-            </ul>
-        </div>
+    <div class="container">
+        <div class="btn-group mb-5" role="group" aria-label="Button group with nested dropdown">
+            <div class="form-outline">
+                <input type="number" id="yearNumber" class="form-control" min="1800"
+                    max="<?php echo date('Y') ?>" value="<?php echo $current_year ?>"/>
+                <label class="form-label" for="yearNumber">Year</label>
+            </div>
+            <div class="btn-group" role="group">
+                <button id="monthNumber" type="button" class="btn btn-primary dropdown-toggle <?php echo !isset($_SESSION['isSignedIn']) ? "disabled" : ""?>"
+                    data-mdb-toggle="dropdown" aria-expanded="false">
+                    <?php echo $current_month ?>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                <?php for($i = 1; $i < count($list_month); $i++){
+                    $selected_month = $list_month[$i]; echo 
+                    "<li onclick=\"funApply('".$selected_month."', '".$i."')\"><a class=\"dropdown-item\" style=\"cursor:pointer;\">".$selected_month."</a></li>"; } ?>
+                </ul>
+            </div>
         </div>
     </div>
-    <?php if($_SESSION['isSignedIn']) { ?>
+    <?php if(isset($_SESSION['isSignedIn'])) { ?>
         <div class="table-responsive">
-            <table id="table" class="table table-striped">
-                <thead class="table-dark"><tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Poliklinik</th>
-                    <?php for($i = 0; $i < $var_date_length; $i++) echo "
-                    <th scope=\"col\" style=\"padding-left:2.5px;padding-right:2.5px\">".str_pad(($i+1), 2, "0", STR_PAD_LEFT)."</th>" ?>
-                    <th scope="col">Total</th>
-                </tr></thead>
+            <table id="table" class="table table-borderless table-hover">
+                <thead class="table-light">
+                    <tr style="vertical-align:middle;text-align:center;">
+                        <th rowspan="2">No</th>
+                        <th rowspan="2">Poliklinik</th>
+                        <th scope="col" colspan="<?php echo $var_date_length?>">Tanggal</th>
+                        <th rowspan="2">Total</th>
+                    </tr>
+                    <tr>
+                        <?php for($i = 0; $i < $var_date_length; $i++) echo "
+                        <th scope=\"col\" style=\"padding-left:2.5px;padding-right:2.5px\">".str_pad(($i+1), 2, "0", STR_PAD_LEFT)."</th>" ?>
+                    </tr>
+                </thead>
                 <?php $num = 0; while($row = $query->fetch_assoc()){ $num++;
                     $param_date = $current_year."-".$current_month_pos; echo "
-                <tbody class=\"table col-xl-12\"> <tr>
-                    <th scope=\"col\">".$num."</th>
-                    <td scope=\"col\" style=\"white-space: nowrap\">".$row['nm_poli']."</td>";
-                    echo funIterateValues($row['kd_poli'], $current_year, $current_month_pos)."
-                </tr>
-                </tbody>"; } ?>
+                <tbody> 
+                    <tr>
+                        <th scope=\"col\">".$num."</th>
+                        <td scope=\"col\" style=\"white-space: nowrap\">".$row['nm_poli']."</td>";
+                        echo funIterateValues($row['kd_poli'], $current_year, $current_month_pos)."
+                    </tr>
+                </tbody>"; 
+                } ?>
+                <tbody class="table bg-light">
+                    <tr>
+                        <th scope="col" colspan="2">Total keseluruhan</th>
+                        <td scope="col" colspan="<?php echo $var_date_length?>"></td>
+                        <th scope="col"><?php echo $total_summary?></th>
+                    </tr>
+                </tbody>
             </table>
     </div>
     <?php } ?>
 
-    <section class="">
+    <section class="footer">
     <footer class="text-center text-white" style="background-color: #0a4275;">
         <div class="container p-4 pb-0">
         <section class="">
@@ -148,7 +160,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
         <form id="login-form" method="post">
-        <?php if(!$_SESSION['isSignedIn']) { ?>
+        <?php if(!isset($_SESSION['isSignedIn'])) { ?>
         <div class="modal-header">
             <h5 class="modal-title" id="staticBackdropLabel">Sign In</h5>
             <button type="button" class="btn-close"
@@ -156,12 +168,12 @@
         </div>
         <div class="modal-body">
         <div class="form-outline">
-            <input id="username" type="text" name="input_text_username" class="form-control" aria-describedby="username-error" required minlength="6" maxlength="20"/>
+            <input id="username" type="text" name="input_text_username" class="form-control" aria-describedby="username-error" required minlength="4" maxlength="20"/>
             <label class="form-label" for="username">Username</label>
         </div>
             <div class="col-auto m-2"></div>
         <div class="form-outline col-auto">
-            <input id="password" type="password" name="input_text_password" class="form-control" aria-describedby="password-error" required minlength="8" maxlength="20"/>
+            <input id="password" type="password" name="input_text_password" class="form-control" aria-describedby="password-error" required minlength="4" maxlength="20"/>
             <label class="form-label" for="password">Password</label>
         </div>
             <div class="col-auto m-2">
@@ -207,17 +219,18 @@
 </body>
 </html>
 <?php
+
 function funSignIn(){
-    $content = array("fadhila", "fadhli", "fitri");
+    $content = array("admin", "fadhila", "fadhli", "fitri");
     
     $username = $_POST['input_text_username'];
     $password = $_POST['input_text_password'];
 
-    if((8 <= strlen($password)) && (strlen($password) <= 20)){
-        if(in_array($username, $content) && $password === "staffonly"){
+    if((4 <= strlen($password)) && (strlen($password) <= 20)){
+        if(in_array($username, $content) && $password === "admin"){
             
             $_SESSION['isSignedIn'] = "true";
-            header("Location: ");
+            header("Location: ./");
         }else header("Location: ?invalid_code=101");
     }else header("Location: ?invalid_code=102");
 }
@@ -226,31 +239,38 @@ function funSignOut(){
     unset($_SESSION['isSignedIn']);
 }
 
+function queryReqPoliCount($kd_poli, $day, $month, $year){
+    global $mysqli;
+
+    return $mysqli->query("SELECT
+        reg_periksa.tgl_registrasi,
+        COUNT(poliklinik.nm_poli) AS reg_poli_count
+        FROM poliklinik 
+        INNER JOIN reg_periksa ON reg_periksa.kd_poli = poliklinik.kd_poli
+
+        WHERE EXTRACT(DAY FROM reg_periksa.tgl_registrasi) = '".$day."'
+        AND EXTRACT(MONTH FROM reg_periksa.tgl_registrasi) = '".$month."'
+        AND EXTRACT(YEAR FROM reg_periksa.tgl_registrasi) = '".$year."'
+        AND reg_periksa.kd_poli = '".$kd_poli."'
+
+        GROUP BY reg_periksa.tgl_registrasi
+    ");
+}
+/* * *
+    iterate horizontal queries
+* */
 function funIterateValues($kd_poli, $year, $month) {
-    global $mysqli, $var_date_length;
+    global $mysqli, $var_date_length, $total_summary;
 
     $total = 0; for($i = 0; $i < $var_date_length; $i++){ $day = ($i+1);
-        $query = $mysqli->query("
-            SELECT
-            reg_periksa.tgl_registrasi,
-            COUNT(poliklinik.nm_poli) AS reg_poli_count
-            FROM poliklinik 
-            INNER JOIN reg_periksa ON reg_periksa.kd_poli = poliklinik.kd_poli
-
-            WHERE EXTRACT(DAY FROM reg_periksa.tgl_registrasi) = '".$day."'
-            AND EXTRACT(MONTH FROM reg_periksa.tgl_registrasi) = '".$month."'
-            AND EXTRACT(YEAR FROM reg_periksa.tgl_registrasi) = '".$year."'
-            AND reg_periksa.kd_poli = '".$kd_poli."'
-
-            GROUP BY reg_periksa.tgl_registrasi
-        ");
-        $reg_poli_count = $query->fetch_assoc()['reg_poli_count'];
+        $reg_poli_count = queryReqPoliCount($kd_poli, $day, $month, $year)->fetch_assoc()['reg_poli_count'];
+        
         if($reg_poli_count > 0){
             $total = $total + $reg_poli_count;
-            echo "<td scope=\"col\" style=\"padding-left:2.5px;padding-right:2.5px\">".$reg_poli_count."</td>";
+            echo "<th scope=\"col\" style=\"padding-left:2.5px;padding-right:2.5px\">".str_pad($reg_poli_count, 2, "0", STR_PAD_LEFT)."</th>";
         }else
-            echo "<td scope=\"col\" style=\"padding-left:2.5px;padding-right:2.5px\">-</td>";
+            echo "<td scope=\"col\" style=\"padding-left:2.5px;padding-right:2.5px\">00</td>";
     }
     echo "<td scope=\"row\">".$total."</td>";
+    $total_summary = $total_summary + $total;
 }
-?>
